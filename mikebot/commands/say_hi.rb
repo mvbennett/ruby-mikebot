0911b805be2cfb7345f7cfa1196e0b6c9593320e
Michael Bennett <mvbennett9@gmail.com>
2022-12-02 01:36:03 -0500

    added another command

--- mikebot/commands/say_hi.rb
+++ mikebot/commands/say_hi.rb
@@ -2,18 +2,38 @@ module Mikebot
   module Commands
     class SayHi < SlackRubyBot::Commands::Base
       command 'say_hi' do |client, data, _match|
-        client.say(channel: data.channel, text: HiText.say_hi)
+        client.say(channel: data.channel, text: Text.say_hi)
       end
 
       command /\w*\scoffee\s\w*/i do |client, data, _match|
-        client.say(channel: data.channel, text: "I'm drinking Haiti coffee from Panther coffee.")
+        client.say(channel: data.channel, text: Text.coffee)
+      end
+
+      command 'american_name' do |client, data, _match|
+        client.say(channel: data.channel, text: Text.american_name)
       end
     end
   end
 end
 
-class HiText
+class Text
   def self.say_hi
     "Hi this is your mikebot, how may I help you?"
+  end
+
+  def self.coffee
+    "I'm drinking Haiti coffee from Panther coffee."
+  end
+
+  def self.american_name
+    [
+      'Sleve McDichael',
+      'Onson Sweemy',
+      'Darryl Archideld',
+      'Anatoli Smorin',
+      'Rey McScriff',
+      'Todd Bonzalez',
+      'Mike Sernandez'
+    ].sample
   end
 end
